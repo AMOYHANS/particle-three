7163dd7e377e3798ff9bf80a35a087197f87e6f2
AMOYHANS <2448005817@qq.com>
2024-03-08 11:50:49 +0800

    图片旋转完成

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ import * as THREE from 'three';
 // import {GUI} from 'three/addons/libs/lil-gui.module.min.js';
 import gsap from 'gsap';
 
-const video = document.getElementById( 'video' );
-video.play();
-const videoTexture = new THREE.VideoTexture( video );
+
+// const video = document.getElementById( 'video' );
+// video.play();
+// const videoTexture = new THREE.VideoTexture( video );
+
+const cubeLoader = new THREE.TextureLoader();
+const cubeTexture = cubeLoader.load('pic1.jpg')
+const cubeTexture2 = cubeLoader.load('pic2.jpg')
+const cubeTexture3 = cubeLoader.load('pic3.jpg')
+const cubeTexture4 = cubeLoader.load('pic4.jpg')
+const cubeTexture5 = cubeLoader.load('pic5.jpg')
 
 const scene = new THREE.Scene();
 
@@ -24,7 +32,7 @@ camera.position.set(0, 3.5, 3.5)
 
 // const gui = new GUI()
 const debugProps = {
-  starNum: 1500,
+  starNum: 1314 + 520,
   starSize: 0.2,
   cubeSize: 8,
   autoRotate: true,
@@ -54,7 +62,7 @@ const colors = []
 function generateStar() {
   for(let i = 0; i < debugProps.starNum; i++){
     positions.push((Math.random() - 0.5) * (debugProps.cubeSize + 8))
-    positions.push((Math.random() - 0.5) * (debugProps.cubeSize + 8))
+    positions.push((Math.random() - 0.5) * (debugProps.cubeSize + 32))
     positions.push((Math.random() - 0.5) * debugProps.cubeSize)
     colors.push(Math.random())
     colors.push(Math.random())
@@ -85,38 +93,51 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(size.width, size.height);
 
-const isMobile = window.innerWidth < 768
+let isMobile = window.innerWidth < 768
 
 const torusGeo = new THREE.BoxGeometry(2, 2, 2)
 const boxMat = new THREE.MeshBasicMaterial({
-  // map: new THREE.TextureLoader().load('avatar.jpg'),
-  map: videoTexture
+  map: cubeTexture,
+  // map: cubeTexture
 })
-const basicMat = new THREE.MeshToonMaterial({ 
-  normalMap: normTexture,
+const basicMat = new THREE.MeshBasicMaterial({ 
+  map: cubeTexture2,
+})
+const basicMat3 = new THREE.MeshBasicMaterial({ 
+  map: cubeTexture3,
+})
+const basicMat4 = new THREE.MeshBasicMaterial({ 
+  map: cubeTexture4,
+})
+const basicMat5 = new THREE.MeshBasicMaterial({ 
+  map: cubeTexture5,
 })
 // gui.addColor(basicMat, 'color')
 const torusMesh = new THREE.Mesh(torusGeo, boxMat)
-const coneGeo = new THREE.ConeGeometry(1, 2, 32)
-const coneMesh = new THREE.Mesh(coneGeo, basicMat)
+const coneMesh = new THREE.Mesh(torusGeo, basicMat)
 coneMesh.position.y = - size.distance
-const torusKnotGeo = new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16)
-const torusKnotMesh = new THREE.Mesh(torusKnotGeo, basicMat)
-torusKnotMesh.position.y = - size.distance * 2
-scene.add(torusMesh,torusKnotMesh,coneMesh)
+const torusKnotMesh = new THREE.Mesh(torusGeo, basicMat3)
+torusKnotMesh.position.y = - size.distance * 1.8
+const torusKnotMesh4 = new THREE.Mesh(torusGeo, basicMat4)
+torusKnotMesh4.position.y = - size.distance * 2.8
+const torusKnotMesh5 = new THREE.Mesh(torusGeo, basicMat5)
+torusKnotMesh5.position.y = - size.distance * 3.8
+scene.add(torusMesh,torusKnotMesh,coneMesh, torusKnotMesh4, torusKnotMesh5)
 if(!isMobile){
   torusMesh.position.x = 2
   coneMesh.position.x = -2
   torusKnotMesh.position.x = 2
+  torusKnotMesh4.position.x = 2
+  torusKnotMesh5.position.x = -2
 }
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
 directionalLight.position.set(2, 4, 2)
 scene.add(directionalLight)
-const sectionMeshes = [torusMesh, coneMesh, torusKnotMesh]
+const sectionMeshes = [torusMesh, coneMesh, torusKnotMesh, torusKnotMesh4, torusKnotMesh5]
 
 const clock = new THREE.Clock()
-const onsectionLength = Math.floor(document.body.scrollHeight / 3)
+const onsectionLength = Math.floor(document.body.scrollHeight / 5.5)
 let currentSection = 0
 let oldTime = clock.getElapsedTime()
 
@@ -161,3 +182,11 @@ window.addEventListener('scroll', () => {
 })
 
 animate()
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
+  isMobile = window.innerWidth < 768
+})
